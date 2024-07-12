0f8724fb05ba657e1ac1ab2e55039d26c965e84d
SanidhyaBaheti <65732300+SanidhyaBaheti@users.noreply.github.com>
2024-07-12 13:00:19 +0530

    Update write_back.v

--- write_back.v
+++ write_back.v
@@ -2,6 +2,7 @@
 `include "regFile.v"
 `include "memoryBank.v"
 `include "pc.v"
+
 module write_back (
     input [4:0] opcode,
     input am,
@@ -17,81 +18,79 @@ module write_back (
     input parity_flag
 );
     reg [15:0] rd_data;
-    reg [7:0] mem_data_in;//data to be saved in memory
+    reg [7:0] mem_data_in; // data to be saved in memory
 
-    wire r_w;
-    wire [7:0] rs1_data,rs2_data,mem_data_out;
+    reg r_w;
+    wire [7:0] rs1_data, rs2_data, mem_data_out;
     reg input_length;
     
     reg loadPC;
     reg incPC;
     reg [5:0] address;
-    wire [5:0] execadd;
 
-    regFile r1 (rs1_data,rs2_data,3'bz,3'bz,rd,rd_data,1'b0,1'b0,input_length);
-    memoryBank m1(mem_data_out,mem_data_in,4'bx,mem_addr,1'b0,1'b0,clk);
-    pc p1(1'b1,1'b0,address,execadd);
+    regFile r1 (rs1_data, rs2_data, 3'bz, 3'bz, rd, rd_data, 1'b0, 1'b0, input_length);
+    memoryBank m1(mem_data_out, mem_data_in, 4'bx, mem_addr, r_w, 1'b0, clk);
+    pc p1(.loadPC(loadPC), .address(address));
 
- parameter MOVE=5'b00000, ADD=5'b00001, SUB=5'b00010, MUL=5'b00011, 
-          DIV=5'b00100, INC=5'b00101, DEC=5'b00110, AND=5'b00111, 
-          OR=5'b01000, NOT=5'b01001, XOR=5'b01010, LOAD=5'b01011, 
-          STORE=5'b01100, JUMP=5'b01101, BEQZ=5'b01110, HALT=5'b11111,      
-          ASHL=5'b10000, ASHR=5'b10001, LSHL=5'b10010, LSHR=5'b10011,       
-          ROTL=5'b10100, ROTR=5'b10101,BC=5'b10110, BAUX=5'b10111,          
-          BPAR=5'b11000, COMPARE=5'b11001  ;                               
+    parameter MOVE = 5'b00000, ADD = 5'b00001, SUB = 5'b00010, MUL = 5'b00011, 
+              DIV = 5'b00100, INC = 5'b00101, DEC = 5'b00110, AND = 5'b00111, 
+              OR = 5'b01000, NOT = 5'b01001, XOR = 5'b01010, LOAD = 5'b01011, 
+              STORE = 5'b01100, JUMP = 5'b01101, BEQZ = 5'b01110, HALT = 5'b11111,      
+              ASHL = 5'b10000, ASHR = 5'b10001, LSHL = 5'b10010, LSHR = 5'b10011,       
+              ROTL = 5'b10100, ROTR = 5'b10101, BC = 5'b10110, BAUX = 5'b10111,          
+              BPAR = 5'b11000, COMPARE = 5'b11001;
 
+    always @(*) begin
+        HALTED <= 0;
+        input_length = 1'b0;
+        loadPC = 1'b0;  
+        r_w = 1'b1;
 
-    always @(*)begin
-        HALTED<=0;
-        input_length=1'b0;
         case(opcode)
-            MOVE, ADD, SUB, AND, OR, XOR,COMPARE,LOAD:
-            begin 
+            MOVE, ADD, SUB, AND, OR, XOR, COMPARE, LOAD: begin 
                 rd_data <= alu_out;
+                if (opcode == LOAD) r_w = 1'b1; 
             end
-            MUL,DIV:
-            begin
-                input_length=1'b1;
+            MUL, DIV: begin
+                input_length = 1'b1;
                 rd_data = alu_out;
             end
-            INC, DEC, NOT,ASHL,ASHR,LSHL,LSHR,ROTL,ROTR:
-            begin
+            INC, DEC, NOT, ASHL, ASHR, LSHL, LSHR, ROTL, ROTR: begin
                 if (am == 1'b0) 
-                   rd_data <= alu_out;
-                else 
-                   mem_data_in <= alu_out;
+                    rd_data <= alu_out;
+                else begin
+                    mem_data_in <= alu_out;
+                    r_w = 1'b0; 
+                end
             end
-            STORE: 
+            STORE: begin
                 mem_data_in <= alu_out;
-            JUMP:
-            begin
+                r_w = 1'b0; 
+            end
+            JUMP: begin
                 address <= instr_mem_addr;
+                loadPC = 1'b1;
             end 
-            BEQZ:
-            if(zero_flag)
-            begin
+            BEQZ: if(zero_flag) begin
                 address <= instr_mem_addr;
+                loadPC = 1'b1;
             end
-            BC:
-            if(carry_flag)
-            begin
+            BC: if(carry_flag) begin
                 address <= instr_mem_addr;
+                loadPC = 1'b1;
             end
-            BAUX:
-            if(auxiliary_flag)
-            begin
+            BAUX: if(auxiliary_flag) begin
                 address <= instr_mem_addr;
+                loadPC = 1'b1;
             end
-            BPAR:
-            if(parity_flag)
-            begin
+            BPAR: if(parity_flag) begin
                 address <= instr_mem_addr;
+                loadPC = 1'b1;
             end    
-            HALT: 
+            HALT: begin
                 HALTED <= 1'b1;
-            default: 
-                ;
-            
+            end
+            default: ;
         endcase
     end
 endmodule
